bb324935e541c005de7772e65a2b8cff755e2a1d
Adriano <adrianoact@msn.com>
2024-06-15 22:07:53 -0300

    add format currency

--- scripts.js
+++ scripts.js
@@ -6,6 +6,20 @@ amount.oninput = () => {
     // Obtém o valor atual do input e remove os caracteres não numéricos
     let value = amount.value.replace(/\D/g, "")
 
+    // Transformar o valor em centatos (exemplo: 150/100 = 1.5 é equivalente a R$ 1,50)
+    value = Number(value) / 100
+
     // Atualiza o valor do input
-    amount.value = value
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    // Retorna o valor formatado
+    return value
 }
